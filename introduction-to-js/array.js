80062b5fcc2a227304ae9140620a42bdd7c3d1df
Sagar Bhandari <rsaagar7200@gmail.com>
2025-02-09 21:43:19 +0545

    adding array

---
+++ introduction-to-js/array.js
@@ -0,0 +1,110 @@
+// javascript arrays
+// const person1 = 'Shyam';
+// const person2 = 'Ram';
+// const person3 = 'Hari';
+// const numbers = [1,2,3,4]
+
+// const persons = [ "Shyam" , "Hari" ]
+
+// const arr = new Array();
+
+
+
+// console.log(arr.length)
+// console.log(numbers.length)
+
+
+
+// console.log(numbers.length)
+
+// console.log(numbers.indexOf(1))
+
+// numbers.push(10)
+// numbers.pop()
+
+
+// const newel = 10;
+// numbers[2] = newel
+
+// const element = numbers[2]
+
+// const element = numbers.shift()
+// numbers.unshift()
+
+
+// console.log('the element is: ',element)
+
+// delete numbers[2]
+// numbers[2]=10;
+
+// console.log(numbers.at(2),numbers[2])
+
+
+// map
+
+// const newArr = numbers.map((el,i)=> el *2)
+
+// const newArr =  numbers.forEach((el,i)=> console.log('The element is:',el))
+
+// filter
+
+// const filteredArr = numbers.filter((el,i) => i !== 2)
+
+// 
+
+
+// const isGtThn3 =  numbers.some((el,i)=> el > 4)
+
+// const iseveryGtThan3 =  numbers.every((el,i)=> el < 5)
+
+// console.log(numbers)
+
+
+
+// console.log(iseveryGtThan3)
+
+
+
+// const persons = [ "Shyam" , "Hari" ]
+
+// console.log(persons.sort())
+
+
+// const numbers = [100,20,30,4,1]
+
+// numbers.sort()
+// console.log(numbers.sort((a,b) => b-a))
+
+// const persons = [
+//     {
+//         name:"shyam",
+//         age:100,
+//         isStudent:false,
+//     },
+//     {
+//         name:"ram",
+//         age:20,
+//         isStudent:false
+
+//     }
+// ]
+
+// persons.sort((a,b)=>  (a.name).localeCompare(b.name) || a.age - b.age )
+
+// console.log(persons)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
