0751efd8c51349a5bb8e8729f892065c1dabf68e
Sagar Bhandari <rsaagar7200@gmail.com>
2025-02-09 22:04:22 +0545

    es6 file

---
+++ introduction-to-js/es6.js
@@ -0,0 +1,82 @@
+// es6
+// ...
+
+const arr =[1,2,3]
+// let arr2 =[3,4,5]
+
+// const arr = arr2.concat(arr2)
+//  arr2 = [...arr2,...arr1]
+//  console.log(arr2)
+
+// const person = {
+//     name:'Jhon',
+//     age:10,
+//     isStudent:false,
+//     isAdult:false
+// }
+
+// const person2 ={...person,isStudent:false}
+
+// console.log(person)
+
+// 
+
+// const {name,age} = person;
+// const [ele1,ele2] = arr
+
+// console.log(name)
+
+// ... rest
+// const {name,age,...others} = person;
+
+
+// function add(...numbers){
+//     let sum = 0
+//    sum = numbers.reduce((acc,val)=>{
+
+//         acc += val
+//         return acc
+//     },0)
+
+//     console.log(sum)
+
+// }
+
+// add(1,2,3,4,5)
+
+// map
+
+
+// const person = {
+//     name:'Jhon',
+//     age:10,
+//     isStudent:false,
+//     isAdult:false
+// }
+
+// var map = new Map()
+// map.set('name',"Jhon");
+// map.set('age',10);
+// map.set(person,'Map with object key')
+
+// // console.log(map)
+
+// console.log(map.has("name"))
+
+
+// set
+// var set = new Set([1,2,3,4,4,3])
+
+// set.add('1')
+// set.add(1)
+// set.add(1)
+
+// console.log(set)
+
+
+// const array = [1,2,3,4,4,3]
+// const array2 = [...new Set(array)]
+
+// console.log(array2)
+
+
