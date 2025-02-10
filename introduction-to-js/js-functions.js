9d0d5d0756b695e6d70adc22878662760a0bb414
Sagar Bhandari <rsaagar7200@gmail.com>
2025-02-10 13:59:16 +0545

    add: js function to new branch

---
+++ introduction-to-js/js-functions.js
@@ -0,0 +1,67 @@
+// function
+
+// function function_name () {
+//     // 
+
+// }
+
+// function greet () {
+//     console.log('hello');
+
+// }
+
+
+
+// greet();
+
+// const greet = function (){
+//     console.log('hello');
+// }
+
+// const hello = "hdh"
+
+// greet()
+
+// const greet = function (name= 'world') {
+//     console.log('hello ' + name);
+//     // console.log(`hello ${name}`);
+// }
+
+// greet('John');
+
+
+// const add = function (a,b) {
+//     return a + b;
+// };
+
+// const  add = (a,b) => a + b;
+
+// const result = add(5,10)
+
+// console.log(result);
+
+
+
+
+// const isMinor = (age) =>{
+//     return age < 18 ? "You are minor" : "You are not minor";
+// }
+
+// const result = isMinor(20);
+// console.log(result);
+
+
+// iife
+
+(function () {
+    console.log('iife');
+
+    
+})()
+
+// (
+// function(){
+
+// }
+// )()
+
